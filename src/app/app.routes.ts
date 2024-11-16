427c748980df0a017ba0c3ca536cf6e6cf7aab3e
BookDev <sivakorn.987@gmail.com>
2024-11-16 22:54:41 +0700

    แก้ไขขนาด bundle และปรับแต่ง CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
     {path:'register',component:RegisterComponent},
-    {path:'dashboard',component:HomeComponent}
+    { path: 'dashboard', loadChildren: () => import('./home/home.component').then(m => m.HomeComponent) }
 ];
 
 @NgModule({
