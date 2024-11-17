87a3a5254611f023c5189ba6f34bbf17108cfe35
BookDev <sivakorn.987@gmail.com>
2024-11-17 15:52:31 +0700

    update angular to v18

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
 // main.ts
 import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
-=======
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import * as cookieParser from 'cookie-parser';
-import * as bodyParser from 'body-parser';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(cookieParser());
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(3000);
-}
-bootstrap();
->>>>>>> 3d2066fff469b9dcc35fcd9a64d8a17795df3597
